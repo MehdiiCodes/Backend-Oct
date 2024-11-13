5fad51ff8067291bee665ff5ce337fa3c68a0411
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-11-13 19:45:32 +0530

    userRouter

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const app = express();
 
 const port = 5000;
 
+//route orendpoint
+app.get('/', (req, res) => {
+    res.send('response from express');
+})
+
+//add
+app.get('/add', (req, res) => {
+    res.send('response from add');
+})
+
+//getall
+app.get('/getall', (req, res) => {
+    res.send('response from getall');
+})
+
+//delete
+app.get('/delete', (req, res) => {
+    res.send('response from delete');
+})
+
 // staring the server
 app.listen( port, () => { console.log('server started');
  } )
