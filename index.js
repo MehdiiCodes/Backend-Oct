4a047f09495aaa621a08dcce23afa571a8f169bc
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-11-19 20:12:34 +0530

    useToaster

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const UserRouter = require('./routers/userRouter');
 const ProductRouter = require('./routers/productRouter');
+const cors = require('cors');
 
 //creating an express app
 const app = express();
@@ -9,9 +10,14 @@ const app = express();
 const port = 5000;
 
 // middleware
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+}));
+
 app.use(express.json());
 app.use( '/user', UserRouter );
-app.use( '/product', ProductRouter )
+app.use( '/product', ProductRouter );
 
 //route orendpoint
 app.get('/', (req, res) => {
