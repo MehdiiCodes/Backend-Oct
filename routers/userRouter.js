ae10bce68f9d50b0466e168918be3f0e80ca7efc
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-11-20 19:24:59 +0530

    error

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,6 +11,8 @@ router.post('/add', (req, res) => {
         .then((result) => {
             res.status(200).json(result);
         }).catch((err) => {
+            console.log(err);
+            
             res.status(500).json(err);
         });
 });
