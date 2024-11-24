3a07fe5618b724d205d1decd932b6ab236f18766
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-11-24 11:55:12 +0530

    WebToken

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Model = require('../models/UserModel');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const router = express.Router();
 
@@ -76,7 +78,27 @@ router.delete('/delete/:id', (req, res) => {
 });
 
 router.post('/authenticate', (req, res) => {
-    Model.findOne(req.body);
+    Model.findOne(req.body)
+    .then((result) => {
+        if (result) {
+            // email and password matched
+            // generate token
+
+            const {_id, email, password} = result;
+            const payload = { _id, email, password }
+
+            jwt.sign(
+                payload,
+                'mytopsecret'
+            )
+
+        } else {
+            res.status(401).json({message: 'Invalid email or password'});
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
 
 module.exports = router;
